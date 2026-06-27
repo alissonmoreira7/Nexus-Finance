7160772e3d8dac653563b363e5a19e0d162ad23b
Alisson Moreira <alonmoreira.dev@gmail.com>
2026-06-27 19:50:12 -0300

    Feature: Aplicação do projeto.

---
+++ src/main/java/com/dev/nexusfinance/NexusFinanceApplication.java
@@ -0,0 +1,13 @@
+package com.dev.nexusfinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusFinanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NexusFinanceApplication.class, args);
+    }
+
+}
