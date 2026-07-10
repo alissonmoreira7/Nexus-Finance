60d9e75e30dd146efb9f3bafcbbe1bbc4fb509b4
Alisson Moreira <alonmoreira.dev@gmail.com>
2026-07-09 22:26:03 -0300

    Bug-Correções: Correções de bugs

--- back-end/src/main/java/com/dev/nexusfinance/repositories/AccountRepository.java
+++ back-end/src/main/java/com/dev/nexusfinance/repositories/AccountRepository.java
@@ -3,6 +3,9 @@ package com.dev.nexusfinance.repositories;
 import com.dev.nexusfinance.models.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
-public interface AccountRepository extends JpaRepository<Account, UUID>{}
+public interface AccountRepository extends JpaRepository<Account, UUID> {
+    List<Account> findByUser_IdUser(UUID userId);
+}
--- back-end/src/main/java/com/dev/nexusfinance/repositories/UserRepository.java
+++ back-end/src/main/java/com/dev/nexusfinance/repositories/UserRepository.java
@@ -3,6 +3,9 @@ package com.dev.nexusfinance.repositories;
 import com.dev.nexusfinance.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
-public interface UserRepository extends JpaRepository<User, UUID> {}
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByCpf(String cpf);
+}
